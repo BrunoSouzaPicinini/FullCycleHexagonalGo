4e63656abc9880e34d8465bc7a179d0046547632
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2023-06-03 05:02:15 -0300

    Test another functions

--- application/product_test.go
+++ application/product_test.go
@@ -2,6 +2,7 @@ package application_test
 
 import (
 	"github.com/bspicinini/go-hexagonal/application"
+	uuid "github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -18,4 +19,42 @@ func TestProduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.NotNil(t, "the price must be greater than zero to enable the product")
+
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+	product.ID = uuid.New().String()
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/bspicinini/go-hexagonal
 
 go 1.20
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/google/uuid v1.3.0
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
